fba799c29f3ed58a7db294c2832b3f162b22e88a
DEMIRR <ludwigsburg100@gmail.com>
2023-07-31 19:26:31 +0200

    first version

---
+++ src/ikinci.java
@@ -0,0 +1,5 @@
+public class ikinci {
+    public static void main(String[] args) {
+        System.out.println("gfhfhhhfhfhfhfhdjdjdjjdjdjjdjddddddddddddddddddddddddddddd");
+    }
+}
---
+++ src/ilkdosya.java
@@ -0,0 +1,7 @@
+public class ilkdosya {
+    public static void main(String[] args) {
+        System.out.println("fjfjfjfjfj" +
+                "f" +
+                "f");
+    }
+}
