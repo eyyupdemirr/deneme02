526f1916ef1bffd48152066ac895bb23c132ad85
DEMIRR <ludwigsburg100@gmail.com>
2023-07-31 19:32:19 +0200

    ikincisatir

--- src/ikinci.java
+++ src/ikinci.java
@@ -1,5 +1,9 @@
 public class ikinci {
     public static void main(String[] args) {
         System.out.println("gfhfhhhfhfhfhfhdjdjdjjdjdjjdjddddddddddddddddddddddddddddd");
+
+
+        System.out.println("ali");
+
     }
 }
